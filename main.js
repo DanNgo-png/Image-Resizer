38b4e64c6c16431c9058580211da058a2f83639a
DanNgo-png <dngo2107@gmail.com>
2025-04-22 00:52:32 -0500

    edit

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const path = require("path");
+const path = require("path"); // loads index.html
 const { app, BrowserWindow} = require("electron");
+
+const isDev = process.env.NODE_ENV !== "development";
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
-        width: 500,
+        width: isDev ? 1000 : 500,
         height: 600
     });
+
+    // Open devtools if in dev env
+    if(isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
-    createMainWindow()
+    createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    };
 });
